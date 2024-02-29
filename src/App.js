c8d0136f61d605d78db49c9aef2a9ab2116765fc
nicoweiss1 <niciweiss@icloud.com>
2024-02-29 13:11:14 +0100

    hh
    ggg

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>hallo</h1>
+      <button onClick={() => alert('Button wurde geklickt!')}>Klick mich</button>
     </div>
   );
 }
